e22c6109f5455a6af75c8c8a2311e21b84ba2480
Nikita Kuznetsov <fa2um@yandex.ru>
2025-11-12 05:39:02 +0300

    add phone mask to input

--- js/app.js
+++ js/app.js
@@ -2,21 +2,26 @@ const app = () => {
   const header = document.querySelector('.header');
   const dateInputs = document.querySelectorAll('.field__input-date');
   const selectInput = document.querySelector('.field__select');
+  const clearFormButton = document.querySelector('.tour-creation__clear-button');
+  const tourCreationForm = document.querySelector('.tour-creation__form');
+  const telInput = document.querySelector('#phonenumber');
   let lastScrollY = window.scrollY;
 
   const today = new Date().toLocaleDateString('en-CA').slice(0, 10);
+  let lastDateInputLength = 0;
 
   const store = {
     dateFromValue: '',
     dateToValue: '',
   };
 
-  // const submitFormButton = document.querySelector('.tour-creation__button');
-  // submitFormButton.addEventListener('click', (e) => {
-  //   selectInput.selectedIndex = 0;
-  // });
-
   const setMinDate = (input) => input.setAttribute('min', today);
+
+  const clearDateInputs = (inputs) => {
+    inputs.forEach((dateInput) => {
+      dateInput.setAttribute('type', 'text');
+    });
+  };
 
   const handleInputDate = (input) => {
     input.addEventListener('focus', () => {
@@ -72,6 +77,84 @@ const app = () => {
 
   header.addEventListener('animationend', (event) => {
     hideFixedHeader(event);
+  });
+
+  clearFormButton.addEventListener('click', () => {
+    tourCreationForm.reset();
+    clearDateInputs(dateInputs);
+  });
+
+  telInput.addEventListener('input', ({ target }) => {
+    const cleanedNumbers = target.value.startsWith('+7') ? target.value.replace(/\D/g, '').slice(1) : target.value.replace(/\D/g, '');
+    const maxLength = 10;
+
+    if (cleanedNumbers.length === maxLength) {
+      telInput.setAttribute('maxlength', '22');
+    } else {
+      telInput.setAttribute('maxlength', '30');
+    }
+
+    if (cleanedNumbers.length === 0) {
+      target.value = '';
+    }
+
+    if (cleanedNumbers.length === 1) {
+      target.value = `+7 (${cleanedNumbers}_ _ ) _ _ _ - _ _ - _ _`;
+      telInput.setSelectionRange(5, 5);
+    }
+
+    if (cleanedNumbers.length === 2) {
+      target.value = `+7 (${cleanedNumbers}_ ) _ _ _ - _ _ - _ _`;
+      telInput.setSelectionRange(6, 6);
+    }
+
+    if (cleanedNumbers.length === 3) {
+      target.value = `+7 (${cleanedNumbers}) _ _ _ - _ _ - _ _`;
+
+      const cursorPosition = lastDateInputLength <= cleanedNumbers.length ? 9 : 7;
+      telInput.setSelectionRange(cursorPosition, cursorPosition);
+    }
+
+    if (cleanedNumbers.length === 4) {
+      target.value = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3)}_ _ - _ _ - _ _`;
+      telInput.setSelectionRange(10, 10);
+    }
+
+    if (cleanedNumbers.length === 5) {
+      target.value = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3)}_ - _ _ - _ _`;
+      telInput.setSelectionRange(11, 11);
+    }
+
+    if (cleanedNumbers.length === 6) {
+      target.value = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3)} - _ _ - _ _`;
+
+      const cursorPosition = lastDateInputLength <= cleanedNumbers.length ? 15 : 12;
+      telInput.setSelectionRange(cursorPosition, cursorPosition);
+    }
+
+    if (cleanedNumbers.length === 7) {
+      target.value = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3, 6)} - ${cleanedNumbers.slice(6)}_ - _ _`;
+      telInput.setSelectionRange(16, 16);
+    }
+
+    if (cleanedNumbers.length === 8) {
+      target.value = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3, 6)} - ${cleanedNumbers.slice(6)} - _ _`;
+
+      const cursorPosition = lastDateInputLength <= cleanedNumbers.length ? 20 : 17;
+      telInput.setSelectionRange(cursorPosition, cursorPosition);
+    }
+
+    if (cleanedNumbers.length === 9) {
+      target.value = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3, 6)} - ${cleanedNumbers.slice(6, 8)} - ${cleanedNumbers.slice(8)}_`;
+      telInput.setSelectionRange(21, 21);
+    }
+
+    if (cleanedNumbers.length === 10) {
+      target.value = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3, 6)} - ${cleanedNumbers.slice(6, 8)} - ${cleanedNumbers.slice(8)}`;
+      telInput.setSelectionRange(22, 22);
+    }
+
+    lastDateInputLength = cleanedNumbers.length;
   });
 };
 
