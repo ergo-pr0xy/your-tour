070619347e001c75b437f3c14c9a2ff2529004e6
Nikita Kuznetsov <fa2um@yandex.ru>
2025-11-27 03:42:41 +0300

    update addPhoneMask.js

--- js/addPhoneMask.js
+++ js/addPhoneMask.js
@@ -1,67 +1,16 @@
-const addPhoneMask = (input, cleanedNumbers, lastInputLength) => {
-  const maxLength = 10;
-  let result = '';
-
-  if (cleanedNumbers.length === 0) {
-    result = '';
-  }
-
-  if (cleanedNumbers.length === 1) {
-    result = `+7 (${cleanedNumbers}_ _ ) _ _ _ - _ _ - _ _`;
-    input.setSelectionRange(5, 5);
-  }
-
-  if (cleanedNumbers.length === 2) {
-    result = `+7 (${cleanedNumbers}_ ) _ _ _ - _ _ - _ _`;
-    input.setSelectionRange(6, 6);
-  }
-
-  if (cleanedNumbers.length === 3) {
-    result = `+7 (${cleanedNumbers}) _ _ _ - _ _ - _ _`;
-    const cursorPosition = lastInputLength < cleanedNumbers.length ? 9 : 7;
-    input.setSelectionRange(cursorPosition, cursorPosition);
-  }
-
-  if (cleanedNumbers.length === 4) {
-    result = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3)}_ _ - _ _ - _ _`;
-    input.setSelectionRange(10, 10);
-  }
-
-  if (cleanedNumbers.length === 5) {
-    result = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3)}_ - _ _ - _ _`;
-    input.setSelectionRange(11, 11);
-  }
-
-  if (cleanedNumbers.length === 6) {
-    result = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3)} - _ _ - _ _`;
-
-    const cursorPosition = lastInputLength < cleanedNumbers.length ? 15 : 12;
-    input.setSelectionRange(cursorPosition, cursorPosition);
-  }
-
-  if (cleanedNumbers.length === 7) {
-    result = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3, 6)} - ${cleanedNumbers.slice(6)}_ - _ _`;
-    input.setSelectionRange(16, 16);
-  }
-
-  if (cleanedNumbers.length === 8) {
-    result = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3, 6)} - ${cleanedNumbers.slice(6)} - _ _`;
-
-    const cursorPosition = lastInputLength < cleanedNumbers.length ? 20 : 17;
-    input.setSelectionRange(cursorPosition, cursorPosition);
-  }
-
-  if (cleanedNumbers.length === 9) {
-    result = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3, 6)} - ${cleanedNumbers.slice(6, 8)} - ${cleanedNumbers.slice(8)}_`;
-    input.setSelectionRange(21, 21);
-  }
-
-  if (cleanedNumbers.length === maxLength) {
-    input.setAttribute('maxlength', '22');
-    result = `+7 (${cleanedNumbers.slice(0, 3)}) ${cleanedNumbers.slice(3, 6)} - ${cleanedNumbers.slice(6, 8)} - ${cleanedNumbers.slice(8)}`;
-    input.setSelectionRange(22, 22);
-  } else {
-    input.setAttribute('maxlength', '30');
+const addPhoneMask = (numbers) => {
+  switch (numbers.length) {
+    case 0: return '+7 (_ _ _) _ _ _ - _ _ - _ _';
+    case 1: return `+7 (${numbers}__) _ _ _ - _ _ - _ _`;
+    case 2: return `+7 (${numbers}_) _ _ _ - _ _ - _ _`;
+    case 3: return `+7 (${numbers}) _ _ _ - _ _ - _ _`;
+    case 4: return `+7 (${numbers.slice(0, 3)}) ${numbers[3]}__ - _ _ - _ _`;
+    case 5: return `+7 (${numbers.slice(0, 3)}) ${numbers.slice(3, 5)}_ - _ _ - _ _`;
+    case 6: return `+7 (${numbers.slice(0, 3)}) ${numbers.slice(3, 6)} - _ _ - _ _`;
+    case 7: return `+7 (${numbers.slice(0, 3)}) ${numbers.slice(3, 6)} - ${numbers[6]}_ - _ _`;
+    case 8: return `+7 (${numbers.slice(0, 3)}) ${numbers.slice(3, 6)} - ${numbers.slice(6, 8)} - __`;
+    case 9: return `+7 (${numbers.slice(0, 3)}) ${numbers.slice(3, 6)} - ${numbers.slice(6, 8)} - ${numbers[8]}_`;
+    default: return `+7 (${numbers.slice(0, 3)}) ${numbers.slice(3, 6)} - ${numbers.slice(6, 8)} - ${numbers.slice(8, 10)}`;
   }
 };
 
--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 import validateForm from './validateForm.js';
+import addPhoneMask from './addPhoneMask.js';
 
 const app = () => {
   const header = document.querySelector('.header');
@@ -410,22 +411,6 @@ const app = () => {
     const normalizedNumbers = state.form.fields.phonenumber.normalized;
     const { phonenumberLastInputLength } = state.uiState;
     handlePhonenumberCursor(normalizedNumbers, phonenumberLastInputLength);
-  };
-
-  const addPhoneMask = (numbers) => {
-    switch (numbers.length) {
-      case 0: return '+7 (_ _ _) _ _ _ - _ _ - _ _';
-      case 1: return `+7 (${numbers}__) _ _ _ - _ _ - _ _`;
-      case 2: return `+7 (${numbers}_) _ _ _ - _ _ - _ _`;
-      case 3: return `+7 (${numbers}) _ _ _ - _ _ - _ _`;
-      case 4: return `+7 (${numbers.slice(0, 3)}) ${numbers[3]}__ - _ _ - _ _`;
-      case 5: return `+7 (${numbers.slice(0, 3)}) ${numbers.slice(3, 5)}_ - _ _ - _ _`;
-      case 6: return `+7 (${numbers.slice(0, 3)}) ${numbers.slice(3, 6)} - _ _ - _ _`;
-      case 7: return `+7 (${numbers.slice(0, 3)}) ${numbers.slice(3, 6)} - ${numbers[6]}_ - _ _`;
-      case 8: return `+7 (${numbers.slice(0, 3)}) ${numbers.slice(3, 6)} - ${numbers.slice(6, 8)} - __`;
-      case 9: return `+7 (${numbers.slice(0, 3)}) ${numbers.slice(3, 6)} - ${numbers.slice(6, 8)} - ${numbers[8]}_`;
-      default: return `+7 (${numbers.slice(0, 3)}) ${numbers.slice(3, 6)} - ${numbers.slice(6, 8)} - ${numbers.slice(8, 10)}`;
-    }
   };
 
   phonenumberInput.addEventListener('input', (e) => {
