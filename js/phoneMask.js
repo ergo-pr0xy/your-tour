07f2bfb3a52abc31bc82fd96e42d88b2905baad0
Nikita Kuznetsov <fa2um@yandex.ru>
2025-11-28 04:00:30 +0300

    refactor phoneMask.js

--- js/phoneMask.js
+++ js/phoneMask.js
@@ -19,51 +19,55 @@ export const handlePhonenumberCursor = (
   lastPhoneValueLength,
   phonenumberInput,
 ) => {
-  if (normalizedNumbers.length === 0) {
-    phonenumberInput.setSelectionRange(4, 4);
-  }
+  const { length } = normalizedNumbers;
 
-  if (normalizedNumbers.length === 1) {
-    phonenumberInput.setSelectionRange(5, 5);
-  }
-
-  if (normalizedNumbers.length === 2) {
-    phonenumberInput.setSelectionRange(6, 6);
-  }
-
-  if (normalizedNumbers.length === 3) {
-    const cursorPosition = lastPhoneValueLength < normalizedNumbers.length ? 9 : 7;
-    phonenumberInput.setSelectionRange(cursorPosition, cursorPosition);
-  }
-
-  if (normalizedNumbers.length === 4) {
-    phonenumberInput.setSelectionRange(10, 10);
-  }
-
-  if (normalizedNumbers.length === 5) {
-    phonenumberInput.setSelectionRange(11, 11);
-  }
-
-  if (normalizedNumbers.length === 6) {
-    const cursorPosition = lastPhoneValueLength < normalizedNumbers.length ? 15 : 12;
-    phonenumberInput.setSelectionRange(cursorPosition, cursorPosition);
-  }
-
-  if (normalizedNumbers.length === 7) {
-    phonenumberInput.setSelectionRange(16, 16);
-  }
-
-  if (normalizedNumbers.length === 8) {
-    const cursorPosition = lastPhoneValueLength < normalizedNumbers.length ? 20 : 17;
-    phonenumberInput.setSelectionRange(cursorPosition, cursorPosition);
-  }
-
-  if (normalizedNumbers.length === 9) {
-    phonenumberInput.setSelectionRange(21, 21);
-  }
-
-  if (normalizedNumbers.length === 10) {
-    phonenumberInput.setSelectionRange(22, 22);
+  switch (length) {
+    case 0: {
+      phonenumberInput.setSelectionRange(4, 4);
+      return;
+    }
+    case 1: {
+      phonenumberInput.setSelectionRange(5, 5);
+      return;
+    }
+    case 2: {
+      phonenumberInput.setSelectionRange(6, 6);
+      return;
+    }
+    case 3: {
+      const cursorPosition = lastPhoneValueLength < normalizedNumbers.length ? 9 : 7;
+      phonenumberInput.setSelectionRange(cursorPosition, cursorPosition);
+      return;
+    }
+    case 4: {
+      phonenumberInput.setSelectionRange(10, 10);
+      return;
+    }
+    case 5: {
+      phonenumberInput.setSelectionRange(11, 11);
+      return;
+    }
+    case 6: {
+      const cursorPosition = lastPhoneValueLength < normalizedNumbers.length ? 15 : 12;
+      phonenumberInput.setSelectionRange(cursorPosition, cursorPosition);
+      return;
+    }
+    case 7: {
+      phonenumberInput.setSelectionRange(16, 16);
+      return;
+    }
+    case 8: {
+      const cursorPosition = lastPhoneValueLength < normalizedNumbers.length ? 20 : 17;
+      phonenumberInput.setSelectionRange(cursorPosition, cursorPosition);
+      return;
+    }
+    case 9: {
+      phonenumberInput.setSelectionRange(21, 21);
+      return;
+    }
+    default: {
+      phonenumberInput.setSelectionRange(22, 22);
+    }
   }
 };
 
